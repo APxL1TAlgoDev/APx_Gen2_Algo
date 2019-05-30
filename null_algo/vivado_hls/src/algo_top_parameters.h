cd3544b0f276653194ee5e79804b966457594c52
Ales Svetek <ales.svetek@cern.ch>
2019-05-30 14:46:15 -0500

    Commit missing header file

---
+++ null_algo/vivado_hls/src/algo_top_parameters.h
@@ -0,0 +1,17 @@
+/**
+ * algo_top_parameters.h
+ *
+ * Defines the algorithm related configuration parameters.
+ */
+
+#ifndef __ALGO_TOP_PARAMETERS_H__
+#define __ALGO_TOP_PARAMETERS_H__
+
+ // Number of input and output links
+ #define N_INPUT_LINKS 24 
+ #define N_OUTPUT_LINKS 24 
+
+// Number of data words per processing cycle/frame
+const int N_WORDS_PER_FRAME	= 6;
+
+#endif /* !__ALGO_TOP_PARAMETERS_H__ */
